33c8240b636b92fabcc0b2e0c27b939284b9b8a2
Quint <mad.kents@hotmail.com>
2018-09-05 16:52:56 +0200

    +test scene for the bumper game +some basic scripts for the ball

---
+++ Assets/Scripts/Ball_Controler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Controler : MonoBehaviour {
+    public Rigidbody rb;
+    float speed = 0.1f;
+
+    bool up;
+    bool down;
+
+	void Update () {
+
+           transform.Translate(0,0, speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bumper1" && down == true)
+        {
+            gameObject.transform.Rotate(0, -90, 0);
+            Debug.Log("blm");
+        }
+
+    }
+
+
+}
