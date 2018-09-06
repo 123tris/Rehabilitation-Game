d46c74ad128167fff87d8fe3e405afb77938fce6
Quint <mad.kents@hotmail.com>
2018-09-06 13:05:25 +0200

    *Bumpers don't spawn at the same place anymore

---
+++ Assets/Scripts/Bumper_placer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper_placer : MonoBehaviour {
+
+    public List<GameObject> Bumpers;
+    public List<Transform> SpawnPoints;
+
+    public int Spawns;
+    public int randomspawn;
+	
+	void Start () {
+        for (int i = 0; i < Spawns; i++)
+        {
+            randomspawn = Random.Range(0, SpawnPoints.Count);
+
+            Transform spawnPosition = SpawnPoints[randomspawn];
+            
+            Instantiate(Bumpers[Random.Range(0, Bumpers.Count)], spawnPosition);
+            SpawnPoints.RemoveAt(randomspawn);
+        }
+
+
+	}
+	
+
+	void Update () {
+		
+	}
+}
