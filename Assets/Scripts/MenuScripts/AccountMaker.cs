ead325bcb5984b40b96423ddbb445ca6acd8a91e
Timur <Timur.d.sel074@gmail.com>
2018-10-08 09:32:48 +0200

    +begin of the menuscripts

--- Assets/Scripts/MenuScripts/AccountMaker.cs
+++ Assets/Scripts/MenuScripts/AccountMaker.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
-//using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 namespace Scripts
 {
-    public class LoginManager : MonoBehaviour
+    public class AccountMaker : MonoBehaviour
     {
 
         public bool accountDoesNotExist;
 
-        public InputField registerAccountName; //Gebruikersnaam en wachtwoord InputField om mee te registreren
+        public InputField registerAccountName; 
         public Text notificationTextOne;
 
         private void Start()
@@ -19,30 +19,25 @@ namespace Scripts
             notificationTextOne.text = "";
         }
 
-        public void DeleteSaves()
-        {
-            PlayerPrefs.DeleteAll();
-        }
 
-        public void CreateAccount() //Maak een account aan
+
+        public void CreateAccount()
         {
-            if (!PlayerPrefs.HasKey("User_" + registerAccountName.text.Trim()) && registerAccountName.text.Length > 0) //Is er geen account met die naam?
+            if (!PlayerPrefs.HasKey("User_" + registerAccountName.text.Trim()) && registerAccountName.text.Length > 0)
             {
-                StopAllCoroutines();
                 StartCoroutine(NotificationTwo("Account is gecreert klik op de rechter pijl om verder te gaan klik niet op een andere knop"));
-                PlayerPrefs.SetString("User_" + registerAccountName.text.Trim(), registerAccountName.text.Trim()); //Sla het account op als 'User_NAAM'
+                PlayerPrefs.SetString("User_" + registerAccountName.text.Trim(), registerAccountName.text.Trim());
                 PlayerPrefs.SetString("User", registerAccountName.text.Trim());
                 accountDoesNotExist = true;
             }
             else
             {
-                StopAllCoroutines();
                 StartCoroutine(NotificationTwo("Account naam bestaat al"));
                 accountDoesNotExist = false;
             }
         }
 
-        private IEnumerator NotificationTwo(string _message) //Laat hier meldingen zien aan de speler
+        private IEnumerator NotificationTwo(string _message)
         {
             notificationTextOne.text = _message;
 
---
+++ Assets/Scripts/UI/StartButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : Button_3D
+{
+    void Start()
+    {
+        buttonPressed.AddListener(OnStart);
+    }
+
+    void OnStart()
+    {
+
+    }
+
+}
