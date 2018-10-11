746f2b723d9a4638df8bd664b1662d0f8d645934
Timur <Timur.d.sel074@gmail.com>
2018-10-11 10:24:54 +0200

    +menu part 1 done part 2 almost done

---
+++ Assets/Scripts/MenuScripts/UI/SwitchPanel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchPanel : Button_3D
+{
+
+    public GameObject panelToEnable;
+    public GameObject panelToDisable;
+
+    void Start()
+    {
+        buttonPressed.AddListener(OnStart);
+    }
+
+    void OnStart()
+    {
+        panelToDisable.SetActive(false);
+        panelToEnable.SetActive(true);
+    }
+
+    public void Switch()
+    {
+        panelToDisable.SetActive(false);
+        panelToEnable.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/MenuScripts/UI/ToggleScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleScript : MonoBehaviour {
+
+
+    Text toggleText;
+
+    public void AccountToggle(bool toggle)
+    {
+        if (toggle == true)
+        {
+            toggleText = gameObject.GetComponentInChildren<Text>();
+            PlayerPrefs.SetString("User", toggleText.ToString());
+        }
+    }
+}
