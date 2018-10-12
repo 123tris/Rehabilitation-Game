0dc84a4c7311343f2a40e888d142f6d0c32ba939
Quint <mad.kents@hotmail.com>
2018-10-12 15:37:00 +0200

    +framework leaderboard.

---
+++ Assets/Scripts/MenuScripts/LeaderBoard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderBoard : AccountMaker
+{
+    public List<Text> Name;
+        
+
+
+    public void Awake()
+    {
+        accountArray = PlayerPrefsX.GetStringArray("Users");
+        accountsList = new List<string>(accountArray);
+        for (int i = 0; i < accountsList.Count; i++)
+        {
+    
+        }
+    }
+}
