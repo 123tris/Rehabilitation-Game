2e0126c2c8802031a37ee31fe41c406400801e05
Collin Bannink <cococol2000@gmail.com>
2018-10-18 13:43:41 +0200

    Made kinect work with prototype

---
+++ Assets/Scripts/Kinect/ImageViewer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageViewer : MonoBehaviour
+{
+    public MeasureDepth mMeasureDepth;
+    public MultiSourceManager mMultiSource;
+
+    public RawImage mRawImage;
+    public RawImage mRawDepth;
+
+    void Update()
+    {
+        mRawImage.texture = mMultiSource.GetColorTexture();
+
+        mRawDepth.texture = mMeasureDepth.mDepthTexture;
+    }
+}
