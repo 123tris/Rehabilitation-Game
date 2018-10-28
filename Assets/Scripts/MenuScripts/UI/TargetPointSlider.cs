8c5d55db596f98bc8c82f8272ced1efc364f0609
Timur <Timur.d.sel074@gmail.com>
2018-10-28 17:13:04 +0100

    +did some stuff in the weekend

---
+++ Assets/Scripts/MenuScripts/UI/TargetPointSlider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetPointSlider : MonoBehaviour {
+
+    public Text pointAmount;
+
+    public void PointAmount(float sliderPo)
+    {
+        PlayerPrefs.SetInt("User_" + PlayerPrefs.GetString("User") + "PointAmount", (int)sliderPo);
+        PlayerPrefs.Save();
+        pointAmount.text = ((int)sliderPo + 2).ToString();
+    }
+}
