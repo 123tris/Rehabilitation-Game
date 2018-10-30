a45500e42b0b338a5db244a935034c53486639e1
Timur <Timur.d.sel074@gmail.com>
2018-10-30 16:51:40 +0100

    *inplemented a bit of the leaderboard

---
+++ Assets/Scripts/Misc/GetScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetScore : LeaderBoard {
+
+    int score;
+    string accountName;
+    int diffuculty;
+
+	public void GiveScore()
+    {
+        AddScore(accountName,diffuculty,score);
+    }
+}
