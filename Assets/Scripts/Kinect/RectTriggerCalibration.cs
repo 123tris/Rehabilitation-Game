c2b6ff87bd7f2e3ba61358516e5da026b4326f55
Collin Bannink <cococol2000@gmail.com>
2018-11-05 16:07:24 +0100

    - making automatic calibration

---
+++ Assets/Scripts/Kinect/RectTriggerCalibration.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using saveGame;
+using System.IO;
+
+public class RectTriggerCalibration : MonoBehaviour
+{
+    private Camera mCamera = null;
+
+    public int motionHit = 0;
+
+    bool calibrationActive = false;
+
+    public Text calibrationText;
+    public GameObject calibrationButton;
+
+    public GameObject[] sliders;
+
+    float timer = 0;
+
+    public MeasureDepthCalibration mdc;
+
+    public PlayerData data;
+
+    string json;
+
+    private void Start()
+    {
+        mdc = FindObjectOfType<MeasureDepthCalibration>();
+        mCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (calibrationActive)
+        {
+            if (mdc.mTriggerPoints.Count >= 50)
+            {
+                timer += Time.deltaTime;
+                //if(timer > 0.01f)
+                {
+                    mdc.mDepthSensitivity -= 0.001f;
+                    mdc.mWallDepth -= 0.01f;
+                }
+            }
+            else
+            {
+                data.SavedWallDepth = mdc.mWallDepth;
+                data.SavedDepthSensitivity = mdc.mDepthSensitivity;
+                data.SavedTopCutOff = mdc.mTopCutOff;
+                data.SavedBottomCutOff = mdc.mBottomCutOff;
+                data.SavedLeftCutOff = mdc.mLeftCutOff;
+                data.SavedRightCutOff = mdc.mRightCutOff;
+                json = JsonUtility.ToJson(data);
+                File.WriteAllText(Application.dataPath + "/StreamingAssets/calibratieFile.json", json);
+                calibrationText.text = "Caliberen klaar!";
+                calibrationButton.SetActive(true);
+                calibrationActive = false;
+            }
+        }
+    }
+
+    public void ActivateCalbration()
+    {
+        foreach(GameObject slider in sliders)
+        {
+            slider.SetActive(false);
+        }
+        calibrationText.text = "";
+        motionHit = 0;
+        calibrationActive = true;
+        calibrationButton.SetActive(false);
+    }
+}
